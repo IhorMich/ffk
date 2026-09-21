d00320fa891bcc819d1b209233c749ae588a26c0
IhorMich <ihor.m.dev@gmail.com>
2026-09-21 14:36:27 +0200

    Change mode from 'local' to 'supabase'

--- js/coach-config.js
+++ js/coach-config.js
@@ -1,5 +1,5 @@
 window.FFK_COACH_CONFIG = {
   supabaseUrl: 'https://iuvggtoamklqhuaswfmi.supabase.co',
   supabaseAnonKey: 'sb_publishable_oSVHv0IEEDr_DvwZu9Kusw_WOSIFg2-',
-  mode: 'local'
+  mode: 'supabase'
 };
